e8aa6787d26f0a4748db55e403da69498f186bc0
HernandezGemmimah <emmbrehernlabdez@gmail.com>
2022-03-25 16:32:48 +0800

    My Personal Info added

--- GitActivity-HernandezGemmimah/git-python-hernandez.py
+++ GitActivity-HernandezGemmimah/git-python-hernandez.py
@@ -1,4 +1,3 @@
 print("Gemmimah Sabida Hernandez")
-print("Purok 5 Mahunig Gasan Marinduque")
-print("September 15 2001")
-print("Bachelor of Science in Information Technology")
+print("Purok-5 Mahunig, Gasan, Marinduque")
+print("September 15, 2001")
